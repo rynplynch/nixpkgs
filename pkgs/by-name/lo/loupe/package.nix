4d5aed131c6152a3553abd55e3e1c6ad5d2b8746
Jan Tojnar <jtojnar@gmail.com>
2025-04-17 19:15:30 +0000

    loupe: 48.0 → 48.1
    https://gitlab.gnome.org/GNOME/loupe/-/compare/48.0...48.1
    
    Changelog-Reviewed-By: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/by-name/lo/loupe/package.nix
+++ pkgs/by-name/lo/loupe/package.nix
@@ -23,11 +23,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "loupe";
-  version = "48.0";
+  version = "48.1";
 
   src = fetchurl {
     url = "mirror://gnome/sources/loupe/${lib.versions.major finalAttrs.version}/loupe-${finalAttrs.version}.tar.xz";
-    hash = "sha256-IUkrI4LrqGuXVxtMwIwesCAc5elr0rm1MziT+F+9HX0=";
+    hash = "sha256-EHE9PpZ4nQd659M4lFKl9sOX3fQ6UMBxy/4tEnJZcN4=";
   };
 
   patches = [
